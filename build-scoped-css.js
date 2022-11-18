61b509567fdb5a192e878d348d2d26754fedaa04
Luke Parker [SSW] <10430890+Hona@users.noreply.github.com>
2022-11-18 11:48:46 +1000

    ✨ Scoped CSS, global CSS & classes in HTML all working!

---
+++ build-scoped-css.js
@@ -0,0 +1,10 @@
+﻿const glob = require('glob');
+const {exec} = require('child_process');
+
+glob("**/*.razor.scss", function (er, files) {
+    files.forEach(function (file) {
+        const command = `npx postcss "${file}" -o "${file.replace('.razor.scss', '.razor.css')}"`;
+        console.log(command)
+        exec(command)
+    });
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{razor,css,scss,cs,html}"],
+  content: ["./**/*.{razor,css,scss,cs,html}"],
   theme: {
     extend: {},
   },
